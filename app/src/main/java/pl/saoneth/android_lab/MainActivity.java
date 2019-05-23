9ce4565415bef6fd2f7e1b9322edc331aff9b5ea
Rafał Stańczyk <rstanczyk@iai-sa.com>
2019-05-23 14:13:50 +0200

    Komentarz

--- app/src/main/java/pl/saoneth/android_lab/MainActivity.java
+++ app/src/main/java/pl/saoneth/android_lab/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // To-do: reszta aplikacji
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
