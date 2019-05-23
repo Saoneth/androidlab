2f4882f19623eb53a443c94b75b73c3290eddc21
Rafał Stańczyk <rstanczyk@iai-sa.com>
2019-05-23 15:31:39 +0200

    poprawka i koniec

--- app/src/main/java/pl/saoneth/android_lab/MainActivity.java
+++ app/src/main/java/pl/saoneth/android_lab/MainActivity.java
@@ -1,7 +1,11 @@
 package pl.saoneth.android_lab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -19,12 +23,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] values = new String[] { "Pies", "Kot", "Koń","Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+        String[] values = new String[]{"Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch"};
         this.target = new ArrayList<>();
         this.target.addAll(Arrays.asList(values));
         this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
 
-        ListView listview = findViewById( R.id.listView );
+        ListView listview = findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi) {
+        Intent intencja = new Intent(this, DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if(requestCode != 1 || resultCode != RESULT_OK) {
+            return;
+        }
+
+        Bundle extras = data.getExtras();
+        String nowy = (String)extras.get("wpis");
+        target.add(nowy);
+        adapter.notifyDataSetChanged();
+    }
+
+
 }
